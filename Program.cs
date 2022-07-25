7d289b6885e6f7ce4b06915b66fd43b3c63b822e
alexandr polotovsky <apolotovsky@gmail.com>
2022-07-25 21:33:21 +0300

    add SelectElements and AmountElements methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк,
+ длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+ либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+ лучше обойтись исключительно массивами.
+Примеры:
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> []
+ */
+
+
+// 1. Создание массива
+// 2. Определение количества элементов массива длиной <= 3
+// 3. Выбор элементов из созданного массива
+// 4. Печать
+
+string[] SelectElements(string[] str)
+{
+    int length = str.Length;
+    int AmountElements(string[] str)
+    {
+        int amountElements = 0;
+        for (int i = 0; i < length; i++)
+        {
+            if (str[i].Length <= 3) amountElements++;
+        }
+        return amountElements;
+    }
+
+    string[] selectedElements = new string[AmountElements(str)];
+    int j = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            selectedElements[j] = str[i];
+            j++;
+        }
+    }
+    return selectedElements;
+}
+
